9c4942e170349ec6fc65b8329501c23f3bd04f4b
Alvin Cherk <alvin@cherk.org>
2022-12-31 16:48:19 +1100

    Add cypress configs

---
+++ cypress/e2e/app.cy.ts
@@ -0,0 +1,8 @@
+describe("empty spec", () => {
+  it("passes", () => {
+    cy.visit("https://example.cypress.io");
+  });
+});
+
+// Prevent TypeScript from reading file as legacy script
+export {};
